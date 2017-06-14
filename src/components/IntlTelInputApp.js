d12784874fb11b3fda63b1c5d8ad2501cf5e541f
Konrad Marzec <kmarzec07@gmail.com>
2017-06-14 11:45:42 +0200

    set flag only if dialcodes are different

--- src/components/IntlTelInputApp.js
+++ src/components/IntlTelInputApp.js
@@ -157,7 +157,9 @@ class IntlTelInputApp extends Component {
       this.setState({
         value: nextProps.value,
       });
-      this.updateFlagFromNumber(nextProps.value);
+      if (this.state.dialCode !== this.getDialCode(nextProps.value)) {
+        this.updateFlagFromNumber(nextProps.value);
+      }
     }
 
     if (this.props.disabled !== nextProps.disabled) {
